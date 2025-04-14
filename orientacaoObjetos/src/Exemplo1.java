4acf7dcc34bc85360c5cd9aa115f0e2bbb0c9275
GABRIEL WEBBER DE SOUZA <04318296083@senacrs.edu.br>
2025-04-14 20:48:28 -0300

    Envio ex

---
+++ orientacaoObjetos/src/Exemplo1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int[] fds = new int[5];
+
+        for(int i = 0; i < fds.length; i++){
+            System.out.println("Digite 5 valores: ");
+            fds[i] = teclado.nextInt();
+
+        }
+        
+        for(int i = 0; i < fds.length; i++){
+          System.out.println(fds[i]);
+        }
+        
+
+
+
+    }
+}
